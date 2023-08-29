2eb1c89fa8216ae2b7b3c92dad224daaae241e24
lnvictor <lnvictor@github.com>
2023-08-29 14:00:16 -0300

    feat: creating properties mapping

---
+++ src/main/java/com/br/geld/configuration/KeyVaultProperties.java
@@ -0,0 +1,43 @@
+package com.br.geld.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "azure.keyvault")
+public class KeyVaultProperties {
+    private String vaultUrl;
+    private String tenantId;
+    private String clientId;
+    private String clientSecret;
+
+    public String getVaultUrl() {
+        return vaultUrl;
+    }
+
+    public void setVaultUrl(String vaultUrl) {
+        this.vaultUrl = vaultUrl;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+}
