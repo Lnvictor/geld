97161466cfe45b67eb642d189f461f8abc110f88
lnvictor <lnvictor@github.com>
2023-09-01 14:34:06 -0300

    Order transfer Object

---
+++ src/main/java/com/br/geld/dto/OrderDTO.java
@@ -0,0 +1,52 @@
+package com.br.geld.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+public class OrderDTO {
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @JsonIgnore
+    private long id;
+    private BigDecimal value;
+
+    private int installmentsNumber;
+
+    private Timestamp createdAt;
+
+    private Timestamp updatedAt;
+
+
+    public OrderDTO(BigDecimal value, int installmentsNumber, Timestamp createdAt, Timestamp updatedAt) {
+        this.value = value;
+        this.installmentsNumber = installmentsNumber;
+        this.createdAt = Timestamp.from(Instant.now());
+        this.updatedAt = Timestamp.from(Instant.now());
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public int getInstallmentsNumber() {
+        return installmentsNumber;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+}
